b6a0ce75d95a14adb28880146ce127710974f118
NaveedNaseem84 <naveed.naseem@nwas.nhs.uk>
2025-02-10 17:53:35 +0000

    create and migrate model and admin for welcome promotions

--- welcome/admin.py
+++ welcome/admin.py
@@ -1,3 +1,14 @@
+"""
+import of libraries
+"""
 from django.contrib import admin
+from .models import WelcomePromotion
 
-# Register your models here.
+@admin.register(WelcomePromotion)
+class PromotionsAdmin(admin.ModelAdmin):
+    """
+    Admin page setup for the promotions.
+    """
+
+    list_display = ('promotion_title', 'is_valid')
+    search_fields = ['promotion_title', 'description']
--- welcome/models.py
+++ welcome/models.py
@@ -1,3 +1,23 @@
+"""
+Import of libraries
+"""
 from django.db import models
 
-# Create your models here.
+VALID_CHOICES = [
+    ("Yes", "Yes"),
+    ("No", "No"),
+    ]
+
+
+class WelcomePromotion(models.Model):
+    """
+    Model structure for the promotions to be
+    displayed on the home page.
+    """
+    promotion_title = models.CharField(max_length=100, blank=False)
+    description = models.TextField(blank=True)
+    is_valid = models.CharField(choices=VALID_CHOICES, blank=False)
+    added_on = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Promotion: {self.promotion_title}"
